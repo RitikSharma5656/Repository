d63ddcd5a1106284874ff767144c49784c3a5d6c
RitikSharma5656 <65593221+RitikSharma5656@users.noreply.github.com>
2021-01-03 19:27:01 +0530

    Create [Binary Search] DAA6.py

---
+++ DAA Practical Document/[Binary Search] DAA6.py
@@ -0,0 +1,20 @@
+n=int(input('enter the size of list'))
+l=[]
+for i in range(0,n):
+    a=int(input('enter element'))
+    l.append(a)
+beg=0
+end=n-1
+mid=int((beg+end)/2)
+b=int(input("enter the search element"))
+while(beg<end and b!=l[mid]):
+    if(b<l[mid]):
+        end=mid-1
+        mid=int((beg+end)/2)
+    else:
+        beg=mid+1
+        mid=int((beg+end)/2)
+if(b==l[mid]):
+    print("position of the search element =",mid+1)
+else:
+    print('not found') 
